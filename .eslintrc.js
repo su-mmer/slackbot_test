3babc5cb5c45fe8546c515e66399335e0dce79b2
su-mmer <heekim987@gmail.com>
2022-12-09 00:12:54 +0900

    install eslint, husky

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    mocha: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
